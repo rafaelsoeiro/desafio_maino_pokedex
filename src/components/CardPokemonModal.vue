7f6e7650afee474a501210f64fcda2838d6d414e
soeiro <rafaelssoeiro@gmail.com>
2024-04-21 02:01:13 -0300

    sparkles: feat: create component modal for show pokemons info and stats

---
+++ src/components/CardPokemonModal.vue
@@ -0,0 +1,34 @@
+<template>
+  <div v-show="isOpen" class="modal" >
+    <button @click="openCloseModal">X</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+  let isOpen=ref(false);
+
+  function openCloseModal(){
+    isOpen.value=!isOpen.value;
+  }
+
+</script>
+
+<style scoped>
+  .modal{
+    z-index: 999;
+    width: 100%;
+    height: 400px;
+    background-color: var(--color-bg-variant);
+    border-radius: 0.5rem;
+    border: 1px solid var(--color-primary);
+    transition: var(--transition);
+  }
+  .modal:hover{
+
+    background-color: transparent;
+
+    border: 1px solid var(--color-primary);
+  }
+</style>
