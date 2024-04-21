34aa42a460b20d131db917f7da8f69c4a31efd42
soeiro <rafaelssoeiro@gmail.com>
2024-04-21 01:50:38 -0300

    sparkles: feat: add attributes on state and create mutations for manage global state

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,28 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    pagination:{
+      limit:18,
+      offset:0,
+    },
+    pokemons:[{}],
+    idioma:[
+      {}
+    ],
   },
   getters: {
   },
   mutations: {
+    setPokemons(state, pokemons) {
+      state.pokemons = pokemons
+    },
+    setIdioma(state, idioma) {
+      state.idioma = idioma
+    },
+    handleLoadMorePokemons(){
+      state.pagination.offset += state.pagination.limit;
+      state.pagination.limit += 18;
+    }
   },
   actions: {
   },
