9b3bdc3fac32935e886d09ccdfed44706ba8a844
soeiro <rafaelssoeiro@gmail.com>
2024-04-21 01:58:37 -0300

    sparkles: feat: populated the pokemons global variable using fetch and add component PokemonList on tamplate

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,25 @@
 <template>
-
   <h1>Home</h1>
+  <PokemonList />
 </template>
 
-<script>
+<script setup>
+  import PokemonList from '@/components/PokemonList.vue';
+  import { onMounted, reactive ,ref} from 'vue';
+  import { useStore } from 'vuex';
 
+  const store = useStore();
+  let pokemons = reactive([]);
+
+
+  const loadPokemons = () => {
+    fetch(`https://pokeapi.co/api/v2/pokemon?limit=${store.state.pagination.limit}&offset=${store.state.pagination.offset}`)
+      .then(res => res.json())
+      .then(data => {
+        store.commit('setPokemons', data.results);
+        pokemons.value = data.results;
+      })
+      .catch(err => console.error(err));
+  };
+  onMounted(loadPokemons);
 </script>
