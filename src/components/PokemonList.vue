504e9c3d5f91432412f2877c5efb8f9c958a3480
soeiro <rafaelssoeiro@gmail.com>
2024-04-22 20:01:14 -0300

    sparkles: feat: finishing implementing infinite scroll

--- src/components/PokemonList.vue
+++ src/components/PokemonList.vue
@@ -1,5 +1,5 @@
 <template>
-  <ul class="cardsContainer">
+  <ul class="cardsContainer" ref="cardsContainer">
     <CardPokemon 
       class="card" 
       v-for="(pokemon, index) in pokemons" 
@@ -8,22 +8,48 @@
       :baseUrl="pokemon.url"
     />
   </ul>
-  <button class="btn jersey-25-regular" @click="loadMorePokemons">Carregar</button>
 </template>
 
 <script setup>
-  import { computed, reactive } from 'vue';
+  import { computed, onBeforeUnmount, onMounted, reactive, ref } from 'vue';
   import { useStore } from 'vuex';
   import CardPokemon from './CardPokemon.vue';
 
   const store = useStore();
-  const pokemons = computed(() => store.state.pokemons);
+  const cardsContainer = ref(null);
+  const pokemons = reactive(computed(() => {
+    return store.state.pokemons
+  }))
+  let lastCardRef = ref(null);
+
+  onMounted(() => {
+    observerLastCart();
+  });
+
+  const observerLastCart = () => {
+    setTimeout(() => {
+      const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            observer.unobserve(lastCardRef.value);
+            loadMorePokemons();
+          } 
+        });
+      });
+      const cards = document.querySelectorAll('.cardsContainer .card');
+      lastCardRef.value = cards[cards.length - 1];
+      if (lastCardRef.value) {
+        observer.observe(lastCardRef.value);
+      }
+    }, 100);
+  }
 
   const loadMorePokemons = () => {
     store.commit('setPagination',store.state.pagination.limit);
     store.dispatch('fetchPokeapi',store.state.pagination.offset)
       .then(() => {
-        console.log('Dados dos pokémons carregados com sucesso! 2');
+        //console.log('Dados dos pokémons carregados com sucesso! 2');
+        observerLastCart();
       })
       .catch(error => {
         console.error('Erro ao carregar os dados dos pokémons:', error);
