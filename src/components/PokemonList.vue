87fddb880d7247ad6062dfda51287f6220ef512c
soeiro <rafaelssoeiro@gmail.com>
2024-04-21 23:57:12 -0300

    :lipstick: feat: refactor css

--- src/components/PokemonList.vue
+++ src/components/PokemonList.vue
@@ -2,8 +2,13 @@
   <div>
     <div class="cardsContainer">
       <div v-for="pokemon in pokemons" :key="pokemon.name" class="card">
-        <h3>{{ pokemon.name }}</h3>
-        <img :src="getPokemonImageUrl(pokemon)" :alt="pokemon.name">
+        <div class="cardImageContainer">
+          <img :src="getPokemonImageUrl(pokemon)" :alt="pokemon.name">
+        </div>
+        <div class="cardContainerTitle">
+          <h3 class="jersey-25-regular">{{ pokemon.name }}</h3>
+          <button class="btnCardPokemon" @click="loadPokemonInfo(pokemon.url)">Info</button>
+        </div> 
       </div>
     </div>
     <button class="btn jersey-25-regular">Carregar</button>
@@ -32,33 +37,89 @@
     }
     return ''; 
   };
+
+  const loadPokemonInfo = (pokemonUrl) => {
+    fetch(pokemonUrl)
+    .then(res => res.json())
+    .then(pokemonInfo=>{
+      console.log(pokemonInfo)
+      return {abilites:pokemonInfo.abilities,moves: pokemonInfo.moves,species:pokemonInfo.species}
+    })
+  }
 </script>
 
 <style scoped>
-  .cardsContainer{
+  .cardsContainer {
     display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
-    column-gap: 2rem;
-    row-gap: 3rem;
+    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+    gap: 20px;
+    padding: 20px;
   }
-  .card{
-    padding: 0.5rem;
-    border-radius: 0.5rem;
+
+  .card {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    max-height: 300px;
+    padding: 0;
     border: 1px solid var(--color-primary);
+    border-radius: 0.5rem;
     background: var(--color-bg-variant);
     text-align: center;
-    cursor: pointer;
     transition: var(--transition);
+    overflow: hidden;
   }
-  .card:hover{
+
+  .card:hover {
     background: transparent;
-    border: 1px solid var(--color-primary-variant);
-    transform: scale(1.1);
-    /* transform: rotateY(180deg); */
-    backface-visibility: hidden;
-    
+    border-color: var(--color-primary-variant);
+    transform: scale(1.05);
   }
-  .card h3{
-    margin-bottom: 1rem;
+
+  .cardImageContainer {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 200px;
+    padding: 1rem;
+  }
+
+  .cardImageContainer img {
+    max-width: 100%;
+    max-height: 100%;
+  }
+
+  .cardContainerTitle {
+    background: var(--color-primary);
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 1rem;
+    padding: 0;
+    max-height: 50px;
+    width: 100%;
+  }
+
+  .cardContainerTitle h3{
+    margin: 0 auto;
+    font-size: 16px;
+    text-transform: uppercase;
+  }
+  .btnCardPokemon{
+    display: inline-block;
+    width: max-content;
+    padding: 0.75rem 1.2rem;
+    background-color: var(--color-primary);
+    color: var(--color-white);
+    cursor: pointer;
+    border: 1px solid var(--color-primary);
+    transition: var(--transition);
+    font-family: "Jersey 25", sans-serif;
+  }
+
+  .btnCardPokemon:hover {
+    background: var(--color-primary-variant);
+    border-color: var(--color-primary-variant);
   }
 </style>
